a4812d0a29294bf86a5c410b55b73376e5f50281
Paul Krause <pkrause@investsoftech.com>
2013-06-21 10:55:48 -0400

    refactor test for fluency
    Make it easier to add more tests

--- src/test/java/hudson/scm/SubversionRepositoryStatusCloudForgeTest.java
+++ src/test/java/hudson/scm/SubversionRepositoryStatusCloudForgeTest.java
@@ -4,12 +4,16 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import hudson.model.Build;
+import hudson.model.ItemGroup;
 import hudson.model.AbstractProject;
+import hudson.model.Project;
 import hudson.scm.SubversionRepositoryStatus.JobProvider;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -22,35 +26,65 @@ import org.kohsuke.stapler.StaplerResponse;
  * @see SubversionRepositoryStatusTest
  */
 public class SubversionRepositoryStatusCloudForgeTest {
-    
-    @SuppressWarnings("rawtypes")
-    @Test
-    @Bug(15794)
-    public void shouldIgnoreDisabledJobs() throws IOException {
-        SubversionRepositoryStatusCloudForge repositoryStatus = new SubversionRepositoryStatusCloudForge("cloudforge.com");
 
-        // GIVEN: a disabled project
-        final AbstractProject project = mock(AbstractProject.class);
-        when(project.isDisabled()).thenReturn(true);
-        
-        JobProvider jobProvider = new JobProvider() {
-            @Override
-            public List<AbstractProject> getAllJobs() {
-                return Collections.singletonList(project);
-            }
-        };
-        
-        repositoryStatus.setJobProvider(jobProvider);
-        
-        // WHEN: post-commit hook is triggered
-        StaplerRequest request = mock(StaplerRequest.class);
-        when(request.getReader()).thenReturn(new BufferedReader(new StringReader("/somepath\n")));
-        StaplerResponse response = mock(StaplerResponse.class);
-        
-        repositoryStatus.doNotifyCommit(request, response);
-        
-        // THEN: disabled project is not considered at all
-        verify(project, never()).getScm();
-    }
+	@SuppressWarnings("rawtypes")
+	static class JobProviderBean implements JobProvider {
+		private List<AbstractProject> allJobs = new ArrayList<AbstractProject>();
+
+		public List<AbstractProject> getAllJobs() {
+			return Collections.unmodifiableList(allJobs);
+		}
+
+		public void addJob(AbstractProject job) {
+			allJobs.add(job);
+		}
+	}
+	
+	static abstract class MockBuild extends Build<MockProject, MockBuild> {
+
+		protected MockBuild(MockProject project) throws IOException {
+			super(project);
+		}
+		
+	}
+	
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	static abstract class MockProject extends Project<MockProject, MockBuild> {
+
+		protected MockProject(ItemGroup parent, String name) {
+			super(parent, name);
+		}
+
+	}
+
+	static BufferedReader asReader(String string) {
+		return new BufferedReader(new StringReader(string));
+	}
+
+	private SubversionRepositoryStatusCloudForge repositoryStatus = 
+			new SubversionRepositoryStatusCloudForge("cloudforge.com");
+	private JobProviderBean jobProvider = new JobProviderBean();
+
+	@Test
+	@Bug(15794)
+	public void shouldIgnoreDisabledJobs() throws IOException {
+
+		// GIVEN: a disabled project
+		final MockProject project = mock(MockProject.class);
+		when(project.isDisabled()).thenReturn(true);
+
+		jobProvider.addJob(project);
+		repositoryStatus.setJobProvider(jobProvider);
+
+		// WHEN: post-commit hook is triggered
+		StaplerRequest request = mock(StaplerRequest.class);
+		when(request.getReader()).thenReturn(asReader("/somepath\n"));
+		StaplerResponse response = mock(StaplerResponse.class);
+
+		repositoryStatus.doNotifyCommit(request, response);
+
+		// THEN: disabled project is not considered at all
+		verify(project, never()).getScm();
+	}
 
 }
